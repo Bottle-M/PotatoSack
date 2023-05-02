44127016a11a39f7df6afc3f44cd7146e1553ce0
SomeBottle <somebottle@outlook.com>
2023-05-02 21:34:22 +0800

    Use okhttp

--- src/main/java/indi/somebottle/potatosack/onedrive/TokenFetcher.java
+++ src/main/java/indi/somebottle/potatosack/onedrive/TokenFetcher.java
@@ -1,5 +1,26 @@
 package indi.somebottle.potatosack.onedrive;
 
-public class TokenFetcher {
 
+
+public class TokenFetcher {
+    private final String endPoint = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+    private final String clientId;
+    private final String clientScrt;
+    private String refreshToken;
+    private String accessToken;
+
+    public TokenFetcher(String clientId, String clientSecret, String refreshToken) {
+        this.clientId = clientId;
+        this.clientScrt = clientSecret;
+        this.refreshToken = refreshToken;
+        this.accessToken = "";
+    }
+
+    public void fetch() {
+
+    }
+
+    public void setRefreshToken(String token) {
+        refreshToken = token;
+    }
 }
